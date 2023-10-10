d6d7a129be41e928c96f203c04855ac065da1556
sakib6619 <sahadothosain6619@gmail.com>
2023-10-11 04:26:43 +0600

    login singup page done authentication

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login,register,forgotten
+from .views import login,register,logout,forgotten
 urlpatterns = [
     path('login/',login, name='login'),
     path('register/',register, name='register'),
+    path('logout/',logout, name='logout'),
     path('forgotten /',forgotten, name='forgotten'),
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 # Login 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.warning(request, "Wrong input")
+            return redirect('login')
     return render(request, 'Accounts/login.html')
 # Register 
 def register(request):
@@ -17,23 +29,31 @@ def register(request):
         password1 = request.POST['password1']
         if len(password) == 0 and len(password1) == 0:
             messages.warning(request, "Input Password !!!")
+        elif len(password) < 8:
+            messages.warning(request, "Password at least 8 character")
         else:
-            if password == password1:
-                if User.objects.filter(username=userName).exists():
-                    messages.warning(request, "Username Already Taken !!!")
-                elif User.objects.filter(email=email).exists():
-                    messages.warning(request, "email Already Taken !!!")
-                else:
-                    user = User.objects.create_user(first_name=firstName, last_name=lastName, username=userName,
-                                                    email=email, password=password)
-                    user.set_password(password)
-                    user.save()
-                    messages.success(request, "User Created !!!")
-                    return redirect('login')
+            if password:
+                pass
             else:
-                messages.warning(request, "Password not matched !!!")
+                if password == password1:
+                    if User.objects.filter(username=userName).exists():
+                        messages.warning(request, "Username Already Taken !!!")
+                    elif User.objects.filter(email=email).exists():
+                        messages.warning(request, "email Already Taken !!!")
+                    else:
+                        user = User.objects.create_user(first_name=firstName, last_name=lastName, username=userName,
+                                                        email=email, password=password)
+                        user.set_password(password)
+                        user.save()
+                        messages.success(request, "User Created !!!")
+                        return redirect('login')
+                else:
+                    messages.warning(request, "Password not matched !!!")
 
     return render(request, 'accounts/register.html')
-
+def logout(request):
+    auth.logout(request)
+    messages.warning(request, "logout successful")
+    return redirect('login')
 def forgotten(request):
     return render(request, 'Accounts/forgotten.html')
