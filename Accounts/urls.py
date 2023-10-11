3050965ab2fe918237da325d9c0c003a80d204e1
sakib6619 <sahadothosain6619@gmail.com>
2023-10-12 05:10:48 +0600

    email verify

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import login,register,logout,forgotten
+from .views import *
 urlpatterns = [
     path('login/',login, name='login'),
     path('register/',register, name='register'),
     path('logout/',logout, name='logout'),
-    path('forgotten /',forgotten, name='forgotten'),
+    path('forgotten/',forgotten, name='forgotten'),
+    path('success/',success, name='success'),
+    path('verify/<auth_token>/',verify, name='verify'),
+    path('fail/',fail, name='fail'),
 ]
